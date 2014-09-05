bdc7f35c716f656ad73eab4e77d302b8af240c46
hertsch <ralf.hertsch@phpmanufaktur.de>
2014-09-05 03:50:59 +0200

    improved #hashtag check in content

--- Control/Command/Tools.php
+++ Control/Command/Tools.php
@@ -101,41 +101,45 @@ class Tools
         preg_match_all('%(?!<a[^>]*?>)((\B##(\w{2,64}\b))|(\B#(\w{2,64}\b)))(?![^<]*?</a>)%i', $content, $matches, PREG_SET_ORDER);
         foreach ($matches as $match) {
             if (isset($match[5])) {
-                if (preg_match('/([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})/i', $match[5]) == 1) {
+                if (((strlen($match[5]) === 3) || strlen($match[5]) === 6) &&
+                    (preg_match('/([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})/i', $match[5]) === 1)) {
                     // ignore hexadecimal color codes ...
                     continue;
                 }
-              if (in_array($match[0], $ignore_hashtags)) {
-                // ignore this already processed hashtag!
-                continue;
-              }
-              $tag_name = str_replace('_', ' ', $match[5]);
-              if (false !== ($tag = $this->TagType->selectByName($tag_name, $language))) {
-                  if ($this->Tag->isAssigned($tag['tag_id'])) {
-                      // replace #tag with a link
-                      $search = array('{link}','{description}','{tag}');
-                      $replace = array(
-                          $this->getPermalinkBaseURL($language).'/buzzword/'.$tag['tag_permalink'],
-                          (!empty($tag['tag_description'])) ? $this->app['utils']->Ellipsis($tag['tag_description'], $ellipsis) : $tag['tag_name'],
-                          ($remove_sharp) ? $tag['tag_name'] : '#'.$tag['tag_name']
-                      );
-                      $tag_link = str_ireplace($search, $replace, $link_replacement);
-                  }
-                  else {
-                      // this #tag is not assigned with any content
-                      $tag_link = str_ireplace('{tag}', '#'.$tag_name, $unassigned_replacement);
-                  }
-              }
-              else {
-                  // invalid #tag
-                  $tag_link = str_ireplace('{tag}', '#'.$tag_name, $invalid_replacement);
-              }
-              $content = str_replace($match[0], $tag_link, $content);
+
+                if (in_array($match[0], $ignore_hashtags)) {
+                    // ignore this already processed hashtag!
+                    continue;
+                }
+
+                $tag_name = str_replace('_', ' ', $match[5]);
+
+                if (false !== ($tag = $this->TagType->selectByName($tag_name, $language))) {
+                    if ($this->Tag->isAssigned($tag['tag_id'])) {
+                        // replace #tag with a link
+                        $search = array('{link}','{description}','{tag}');
+                        $replace = array(
+                            $this->getPermalinkBaseURL($language).'/buzzword/'.$tag['tag_permalink'],
+                            (!empty($tag['tag_description'])) ? $this->app['utils']->Ellipsis($tag['tag_description'], $ellipsis) : $tag['tag_name'],
+                            ($remove_sharp) ? $tag['tag_name'] : '#'.$tag['tag_name']
+                        );
+                        $tag_link = str_ireplace($search, $replace, $link_replacement);
+                    }
+                    else {
+                        // this #tag is not assigned with any content
+                        $tag_link = str_ireplace('{tag}', '#'.$tag_name, $unassigned_replacement);
+                    }
+                }
+                else {
+                    // invalid #tag
+                    $tag_link = str_ireplace('{tag}', '#'.$tag_name, $invalid_replacement);
+                }
+                $content = str_replace($match[0], $tag_link, $content);
             }
             else {
-              // this is a ## marked item which has to be ignored - so we remove only the first # ...
-              $ignore_hashtags[] = substr($match[0], 1);
-              $content = str_replace($match[0], substr($match[0], 1), $content);
+                // this is a ## marked item which has to be ignored - so we remove only the first # ...
+                $ignore_hashtags[] = substr($match[0], 1);
+                $content = str_replace($match[0], substr($match[0], 1), $content);
             }
         }
         return $content;
